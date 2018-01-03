ebb5e5e708735fd29c0374cd4e16265393f8ce65
Sean Davis <seandavi@gmail.com>
2018-01-03 12:54:50 -0500

    rescue missing xml records using livelist csv file

--- scripts/sra_entity_to_json.py
+++ scripts/sra_entity_to_json.py
@@ -7,16 +7,42 @@ parser = argparse.ArgumentParser("Dump an SRA xml file as json")
 
 parser.add_argument('fname',
                     help="The SRA xml filename")
+parser.add_argument('--livelist',
+                    help="the live list csv file")
 
 opts = parser.parse_args()
 
 if('study' in opts.fname):
+    entity = "STUDY"
     sra_parser = omicidx.sra_parsers.study_parser
 if('run' in opts.fname):
+    entity = "RUN"
     sra_parser = omicidx.sra_parsers.run_parser
 if('sample' in opts.fname):
+    entity = "SAMPLE"
     sra_parser = omicidx.sra_parsers.sample_parser
 if('experiment' in opts.fname):
+    entity = "EXPERIMENT"
     sra_parser = omicidx.sra_parsers.experiment_parser
+if(opts.livelist):
+    livelist={}
+    for row in omicidx.sra_parsers.parse_livelist(opts.livelist):
+        if(row['Type']!=entity):
+            continue
+        # this accession stuff is just to make the
+        # names match up when merging.
+        accession = row['Accession']
+        del(row['Accession'])
+        row['accession'] = accession
+        livelist[accession]=row
 for rec in sra_parser(opts.fname):
+    rec = {**rec, **livelist[rec['accession']]}
+    # mark those livelist entries already used
+    livelist[rec['accession']]['used']=True
     print(json.dumps(rec, indent=4))
+for llrec in livelist.values():
+    if('used' in llrec):
+        continue
+    print(json.dumps(llrec, indent=4))
+    
+    

5f7c6a6c41e7020cb2cc4c0f05f82daca5a4175a
Sean Davis <seandavi@gmail.com>
2018-02-12 14:51:15 -0500

    add parser for xml node

--- omicidx/sra_parsers.py
+++ omicidx/sra_parsers.py
@@ -25,6 +25,8 @@ import datetime
 import os
 import json
 import csv
+import urllib
+import xml.etree.ElementTree as etree
 from omicidx.utils import open_file
 
 def _safe_add_text_element(d, key, elem):
@@ -226,7 +228,10 @@ def experiment_parser(fname):
             read_spec['read_index'] = int(elem.find('.//READ_INDEX').text)
             read_spec['read_class'] = elem.find('.//READ_CLASS').text
             read_spec['read_type'] = elem.find('.//READ_TYPE').text
-            read_spec['base_coord'] = int(elem.find('.//BASE_COORD').text)
+            try:
+                read_spec['base_coord'] = int(elem.find('.//BASE_COORD').text)
+            except:
+                pass
             d['read_spec'].append(read_spec)
         if(elem.tag == "DESIGN_DESCRIPTION" and event == 'end'):
             _safe_add_text_element(d, 'design_description', elem)
@@ -248,6 +253,186 @@ def experiment_parser(fname):
             d['instrument_model'] = elem.find('.//INSTRUMENT_MODEL').text
         if(elem.tag == 'EXPERIMENT_ATTRIBUTE' and event == 'end'):
             _add_attributes(d, elem)
+
+
+def _study_xml_iter_parser(xml):
+    """Parse an SRA xml STUDY element
+
+    Parameters
+    ----------
+    xml: an xml.etree Element
+
+    Returns
+    -------
+    A dict object parsed from the XML
+    """
+
+    d = {}
+    d.update(xml.attrib)
+    for elem in xml.iter():
+        if(elem.tag == 'PRIMARY_ID' ):
+            d['accession'] = elem.text
+        if(elem.tag == 'EXTERNAL_ID' ):
+            d['external_id'] = {}
+            d['external_id']['id'] = elem.text
+            d['external_id']['namespace'] = elem.attrib['namespace']
+            if(d['external_id']['namespace'] == 'BioProject'):
+                d['BioProject'] = elem.text
+            if(d['external_id']['namespace'] == 'dbGaP'):
+                d['dbGaP'] = elem.text
+            if(d['external_id']['namespace'] == 'GEO'):
+                d['GEO'] = elem.text
+        if(elem.tag == 'SUBMITTER_ID' ):
+            d['submitter_id'] = {}
+            d['submitter_id']['id'] = elem.text
+            d['submitter_id']['namespace'] = elem.attrib['namespace']
+        if(elem.tag == 'STUDY_TITLE' ):
+            d['title'] = elem.text
+        if(elem.tag == 'STUDY_ABSTRACT' ):
+            d['abstract'] = elem.text
+        if(elem.tag == 'STUDY_DESCRIPTION' ):
+            d['description'] = elem.text
+        if(elem.tag == 'STUDY_ATTRIBUTE' ):
+            _add_attributes(d, elem)
+    return(d)
+
+def _run_xml_iter_parser(xml):
+    """Parse an SRA xml RUN element
+
+    Parameters
+    ----------
+    xml: an xml.etree Element
+
+    Returns
+    -------
+    A dict object parsed from the XML
+    """
+    d = {}
+    d.update(xml.attrib)
+    for elem in xml.iter():
+        if(elem.tag == 'PRIMARY_ID' ):
+            d['accession'] = elem.text
+        if(elem.tag == 'EXPERIMENT_REF' ):
+            d['experiment_accession'] = elem.attrib['accession']
+        if(elem.tag == 'TITLE' ):
+            d['title'] = elem.text
+        if(elem.tag == 'RUN_ATTRIBUTE' ):
+            _add_attributes(d, elem)
+    return d
+
+            
+def _experiment_xml_iter_parser(xml):
+    """Parse an SRA xml EXPERIMENT element
+
+    Parameters
+    ----------
+    xml: an xml.etree Element
+
+    Returns
+    -------
+    A dict object parsed from the XML
+    """
+    d = {}
+    library_tags = [
+        "LIBRARY_STRATEGY",
+        "LIBRARY_SOURCE",
+        "LIBRARY_SELECTION",
+        "LIBRARY_CONSTRUCTION_PROTOCOL",
+        "LIBRARY_NAME"
+        ]
+    for elem in xml.iter():
+        if(elem.tag == 'STUDY_REF'):
+            try:
+                d['study_accession'] = elem.attrib['accession']
+            except:
+                pass
+        if(elem.tag == 'SUBMITTER_ID' ):
+            db = elem.attrib['namespace'].lower()
+            if(db == 'geo'):
+                d[db + '_accession'] = elem.text
+        if(elem.tag == 'TITLE' ):
+            d['title'] = elem.text
+        if(elem.tag =='EXPERIMENT' ):
+            d = {}
+        if(elem.tag =='READ_SPEC' ):
+            if('read_spec' not in d):
+                d['read_spec']=[]
+            read_spec = {}
+            read_spec['read_index'] = int(elem.find('.//READ_INDEX').text)
+            read_spec['read_class'] = elem.find('.//READ_CLASS').text
+            read_spec['read_type'] = elem.find('.//READ_TYPE').text
+            try:
+                read_spec['base_coord'] = int(elem.find('.//BASE_COORD').text)
+            except:
+                pass
+            d['read_spec'].append(read_spec)
+        if(elem.tag == "DESIGN_DESCRIPTION" ):
+            _safe_add_text_element(d, 'design_description', elem)
+        if(elem.tag == "SAMPLE_DESCRIPTOR" ):
+            try:
+                d['sample_accession'] = elem.attrib['accession']
+            except:
+                pass
+        if(elem.tag in library_tags ):
+            _safe_add_text_element(d, elem.tag.lower(), elem)
+        if(elem.tag == "PAIRED" ):
+            d['paired'] = True
+        if(elem.tag == "SINGLE" ):
+            d['paired'] = False
+        if(elem.tag == "SPOT_LENGTH" ):
+            d['spot_length'] = int(elem.text)
+        if(elem.tag == "PLATFORM" ):
+            d['platform'] = elem.find('.//INSTRUMENT_MODEL/..').tag
+            d['instrument_model'] = elem.find('.//INSTRUMENT_MODEL').text
+        if(elem.tag == 'EXPERIMENT_ATTRIBUTE' ):
+            _add_attributes(d, elem)
+    return(d)
+
+def _sample_xml_iter_parser(xml):
+    """Parse an SRA xml SAMPLE element
+
+    Parameters
+    ----------
+    xml: an xml.etree Element
+
+    Returns
+    -------
+    A dict object parsed from the XML
+    """
+
+    d = {}
+    d.update(xml.attrib)
+    for elem in xml.iter():
+        if(elem.tag == "TAXON_ID" ):
+            d['taxon_id'] = int(elem.text)
+        if(elem.tag == "SCIENTIFIC_NAME"):
+            d['organism'] = elem.text
+        if(elem.tag == "EXTERNAL_ID"):
+            try:
+                if(elem.attrib['namespace']=='BioSample'):
+                    d['biosample_accession']=elem.text
+                if(elem.attrib['namespace']=='GEO'):
+                    d['geo_accession']=elem.text
+                if(elem.attrib['namespace']=='ArrayExpress'):
+                    d['arrayexpress_accession']=elem.text
+            except:
+                pass
+        if(elem.tag == 'TITLE' ):
+            d['title'] = elem.text
+        if(elem.tag == 'SAMPLE_ATTRIBUTE' ):
+            _add_attributes(d, elem)
+        
+        if(elem.tag == 'DESCRIPTION' ):
+            d['description'] = elem.text
+        if(elem.tag == 'SUBMITTER_ID' ):
+            ns = elem.attrib['namespace']
+            value = elem.text
+            if('submitter_ids' not in d):
+                d['submitter_ids'] = []
+            d['submitter_ids'].append({'namespace' : ns,
+                                       'value': value})
+    return(d)
+
 
 def sample_parser(fname):
     """Parse an SRA sample XML file
@@ -312,8 +497,8 @@ def sample_parser(fname):
             value = elem.text
             if('submitter_ids' not in d):
                 d['submitter_ids'] = []
-            d['submitter_ids'].append({ns :
-                                      value})
+            d['submitter_ids'].append({'namespace' : ns,
+                                       'value': value})
 
 
 def _custom_csv_parser(fname):
@@ -334,3 +519,19 @@ def parse_run_info(fname):
 def parse_addons_info(fname):
     return _custom_csv_parser(fname)
 
+def load_xml_by_accession(accession):
+    with urllib.request.urlopen('https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=FullXml&term={}'.format(accession)) as response:
+        xml = etree.parse(response)
+        return xml
+
+
+import json
+def get_accession_list(from_date=None, count=50, type="EXPERIMENT"):
+    column_names = ["Accession", "Submission", "Type", "Received", "Published", "LastUpdate", "Status", "Insdc", "Meta", "BioSample", "BioProject", "ReplacedBy", "FileSize", "FileMd5", "FileDate"]
+    url = "https://www.ncbi.nlm.nih.gov/Traces/sra/status/srastatrep.fcgi/acc-mirroring?from_date={}&count={}&type={}"
+    url = url.format(from_date,count,type)
+    with urllib.request.urlopen(url) as response:
+        res = json.loads(response.read().decode('UTF-8'))
+        return res
+    
+    

8293c4bc370eb6e7b889504a7facded18484e799
Sean Davis <seandavi@gmail.com>
2018-03-01 15:27:24 -0500

    refactor to single class parser per entity type

--- omicidx/sra_parsers.py
+++ omicidx/sra_parsers.py
@@ -32,6 +32,450 @@ def lambda_handler(event, context):
     return json.dumps(s.data)
 
 
+def parse_study(xml):
+    """Parse an SRA xml STUDY element
+
+    Parameters
+    ----------
+    xml: an xml.etree Element
+
+    Returns
+    -------
+    A dict object parsed from the XML
+    """
+
+    d = {}
+    d.update(xml.attrib)
+    path_map = {
+        'title': (".//STUDY_TITLE", "text"),
+        'abstract': (".//STUDY_ABSTRACT", "text"),
+        'description': (".//STUDY_DESCRIPTION", "text"),
+    }
+    d.update(_process_path_map(xml, path_map))
+    d.update(_parse_identifiers(xml.find("IDENTIFIERS"),"study"))
+    d.update(_parse_study_type(xml.find('DESCRIPTOR/STUDY_TYPE')))
+    d = try_update(d, _parse_attributes(xml.find("STUDY_ATTRIBUTES")))
+    return(d)
+
+
+def parse_submission(xml):
+    """Parse an SRA xml SUBMISSION element
+
+    Parameters
+    ----------
+    xml: xml.etree.ElementTree.Element
+
+    Returns
+    -------
+    a dict of experiment
+    """
+    d = {}
+    d.update(xml.attrib)
+    d.update(_parse_identifiers(xml.find("IDENTIFIERS"), "submission"))
+    return d
+
+
+def parse_run(xml):
+    """Parse an SRA xml RUN element
+
+    Parameters
+    ----------
+    xml: an xml.etree Element
+    include_run_info: boolean
+        Whether or not to make a call out to the run_info api
+
+    Returns
+    -------
+    A dict object parsed from the XML
+    """
+    d = {}
+    path_map = {
+        'title': (".//RUN_TITLE", "text"),
+        'abstract': (".//STUDY_ABSTRACT", "text"),
+        'description': (".//STUDY_DESCRIPTION", "text"),
+        'experiment_accession': (".//EXPERIMENT_REF", "accession")
+    }
+
+    d = try_update(d, _parse_taxon(xml.find("tax_analysis")))
+    d = try_update(d, _parse_run_reads(xml.find("Statistics")))
+    d.update(xml.attrib)
+    d.update(_process_path_map(xml, path_map))
+    d.update(_parse_identifiers(xml.find("IDENTIFIERS"), "run"))
+    d = try_update(d, _parse_attributes(xml.find("RUN_ATTRIBUTES")))
+    return d
+
+
+def parse_experiment(xml):
+    """Parse an SRA xml EXPERIMENT element
+
+    Parameters
+    ----------
+    xml: xml.etree.ElementTree.Element
+
+    Returns
+    -------
+    a dict of experiment
+    """
+    d = {}
+    d.update(xml.attrib)
+
+    path_map = {
+        'title': ('./TITLE', 'text'),
+        'study_accession': ('./STUDY_REF/IDENTIFIERS/PRIMARY_ID', 'text'),
+        'design': ('./DESIGN/DESIGN_DESCRIPTION', 'text'),
+        'library_name': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_NAME', 'text'),
+        'library_strategy': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_STRATEGY', 'text'),
+        'library_source': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_SOURCE', 'text'),
+        'library_selection': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_SELECTION', 'text'),
+        'library_layout': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT',
+            'child', 'tag'),
+        'library_layout_orientation': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT/PAIRED',
+            'ORIENTATION'),
+        'library_layout_length': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT/PAIRED',
+            'NOMINAL_LENGTH'),
+        'library_layout_sdev': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_LAYOUT/PAIRED',
+            'NOMINAL_SDEV'),
+        'pooling_stategy': (
+            './DESIGN/LIBRARY_DESCRIPTOR/POOLING_STRATEGY',
+            'text'),
+        'library_construction_protocol': (
+            './DESIGN/LIBRARY_DESCRIPTOR/LIBRARY_CONSTRUCTION_PROTOCOL',
+            'text'),
+        'platform': ('./PLATFORM', 'child', 'tag'),
+        'instrument_model': ('./PLATFORM/*/INSTRUMENT_MODEL', 'text')
+    }
+
+    d.update(_process_path_map(xml, path_map))
+    d.update(_parse_identifiers(xml.find("IDENTIFIERS"),
+                                     "experiment"))
+    d = try_update(d, _parse_attributes(
+        xml.find("EXPERIMENT_ATTRIBUTES")))
+    return d
+
+
+def parse_sample(xml):
+    """Parse an SRA xml SAMPLE element
+
+    Parameters
+    ----------
+    xml: an xml.etree Element
+
+    Returns
+    -------
+    A dict object parsed from the XML
+    """
+
+    d = {}
+    d.update(xml.attrib)
+    path_map = {
+        'title': (".//TITLE", "text"),
+        'organism': (".//SCIENTIFIC_NAME", "text"),
+        'description': (".//DESCRIPTION", "text"),
+    }
+    d.update(_process_path_map(xml, path_map))
+    d.update(_parse_identifiers(xml.find("IDENTIFIERS"), "sample"))
+    d.update(_parse_attributes(xml.find("SAMPLE_ATTRIBUTES")))
+
+    for elem in xml.iter():
+        if(elem.tag == "TAXON_ID"):
+            d['taxon_id'] = int(elem.text)
+
+    return(d)
+
+
+def _parse_run_reads(node):
+    """Parse reads from runs."""
+    d = dict()
+    d['reads'] = []
+
+    nreads = node.get('nreads')
+    try:
+        if nreads is not None:
+            d['nreads'] = int(nreads)
+    except:
+        # Sometimes nreads is set to 'variable' and no read information is
+        # provided. I set these values to -1.
+        if nreads == 'variable':
+            d['nreads'] = -1
+        else:
+            raise ValueError('nreads is "{}", this value is not expected. '
+                             ''.format(nreads))
+    for read in node.findall('.//Read'):
+        r = {}
+        r['read_len_avg'] = int(read.get('average', 0))
+        r['read_len_stdev'] = int(read.get('stdev', 0))
+        r['count'] = int(read.get('count', 0))
+        r['index'] = int(read.get('index', 0))+1
+        d['reads'].append(r)
+
+    return d
+
+
+def _parse_taxon(node):
+    """Parse taxonomy informaiton."""
+
+    def crawl(node, d=defaultdict(list)):
+        for i in node:
+            rank = i.get('rank', 'Unkown')
+            d[rank].append({
+                'name': i.get('name').replace('.', '_').replace('$', ''),
+                'parent': node.get('name'),
+                'total_count': int(i.get('total_count')),
+                'self_count': int(i.get('self_count')),
+                'tax_id': i.get('tax_id'),
+                })
+            if i.getchildren():
+                d.update(crawl(i, d))
+        return d
+
+    d = {'tax_analysis': {'nspot_analyze': node.get('analyzed_spot_count'),
+                          'total_spots': node.get('total_spot_count'),
+                          'mapped_spots': node.get('identified_spot_count'),
+                          'tax_counts': crawl(node),
+                          },
+         }
+
+    try:
+        if d['tax_analysis']['nspot_analyze'] is not None:
+            d['tax_analysis']['nspot_analyze'] = int(d['tax_analysis']['nspot_analyze'])
+    except:
+        logger.debug('Non integer count: nspot_analyze')
+        logger.debug(d['tax_analysis']['nspot_analyze'])
+        d['tax_analysis']['nspot_analyze'] = None
+
+    try:
+        if d['tax_analysis']['total_spots'] is not None:
+            d['tax_analysis']['total_spots'] = int(d['tax_analysis']['total_spots'])
+    except:
+        logger.debug('Non integer count: total_spots')
+        logger.debug(d['tax_analysis']['total_spots'])
+        d['tax_analysis']['total_spots'] = None
+
+    try:
+        if d['tax_analysis']['mapped_spots'] is not None:
+            d['tax_analysis']['mapped_spots'] = int(d['tax_analysis']['mapped_spots'])
+    except:
+        logger.debug('Non integer count: mapped_spots')
+        logger.debug(d['tax_analysis']['mapped_spots'])
+        d['tax_analysis']['mapped_spots'] = None
+
+    return d
+
+
+def _safe_add_text_element(d, key, elem):
+    """Add text from an xml element to a dict
+
+    Because not all elements have text elements despite 
+    their existence in the xml tree, this little 
+    function checks for text existence and then
+    adds the text conditionally. If no text is present,
+    the key is not created. 
+
+    Parameters
+    ----------
+    d : dict
+        Add the text element to this dict
+    key : str
+        The key to which to add the text element
+    elem : lxml.etree.Element
+        From where to extract the text
+    """
+    txt = elem.text
+    if(txt is not None):
+        d[key] = txt.strip()
+
+
+def _parse_attributes(xml):
+    if(xml is None):
+        return {}
+    """Add attributes to a record
+
+    Parameters
+    ----------
+    xml: xml.etree.ElementTree.ElementTree.Element
+        An xml element of level "EXPERIMENT|STUDY|RUN|SAMPLE_ATTRIBUTES"
+    """
+    d = defaultdict(list)
+    # Iterate over "XXX_ATTRIBUTES"
+    for elem in xml:
+        try:
+            tag = elem.find('./TAG')
+            value = elem.find('./VALUE')
+            d['attributes'].append({'tag': tag.text, 'value': value.text})
+        except AttributeError:
+            # tag or value missing text, so skip
+            pass
+    if(len(d)==0):
+        d={}
+    return(d)
+
+
+def _get_special_ids(id_rec):
+    namespace_map = {"geo": "GEO",
+                     "gds": "GEO_Dataset",
+                     "pubmed": "pubmed",
+                     "biosample": "BioSample",
+                     "bioproject": "BioProject"}
+    # code below from sramongo/sra.py by jtfear
+    # https://github.com/jfear/sramongo/blob/master/sramongo/sra.py
+    # 
+    # Make sure fully formed xref
+    try:
+        _id = id_rec['id']
+        _db = id_rec['namespace']
+    except Exception:
+        return False
+
+    if (_id is None) | (_db is None):
+        return False
+
+    # normalize db name
+    try:
+        norm = _db.strip(' ()[].:').lower()
+    except Exception:
+        norm = ''
+
+    if norm in namespace_map.keys():
+        # Normalize the ids a little
+        id_norm = re.sub('geo|gds|bioproject|biosample|pubmed|pmid',
+                         '', _id.lower()).strip(' :().').upper()
+        return namespace_map[norm], id_norm
+    else:
+        return False
+
+
+def _parse_identifiers(xml, section):
+    """Parse IDENTIFIERS section"""
+
+    d = defaultdict(list)
+
+    for _id in xml:
+        if(_id.tag == "PRIMARY_ID"):
+            d[section + '_accession'] = _id.text
+        elif(_id.tag == "SUBMITTER_ID"):
+            id_rec = {'namespace': _id.get("namespace"), 'id': _id.text}
+            d[_id.tag.lower()].append(id_rec)
+        elif(_id.tag == "UUID"):
+            d[_id.tag.lower()].append(_id.text)
+        else:  # all other id types (secondary, external)
+            id_rec = {'namespace': _id.get("namespace"), 'id': _id.text}
+            d[_id.tag.lower()].append(id_rec)
+            special = _get_special_ids(id_rec)
+            if(special):
+                d[special[0]] = special[1]
+            else:
+                d[_id.tag.lower()].append(id_rec)
+    return(d)
+
+
+def _process_path_map(xml, path_map):
+    d = {}
+    for k, v in path_map.items():
+        try:
+            # use "text" as second tuple value to
+            # get the text value
+            if(v[1] == 'text'):
+                d[k] = xml.find(v[0]).text
+                # use the name of the attribute to
+                # get a specific attribute
+            elif(v[1] == 'child'):
+                child = xml.find(v[0]).getchildren()
+
+                if len(child) > 1:
+                    raise Exception(
+                        'There are too many elements')
+                elif v[2] == 'text':
+                    d[k] = child[0].text
+                elif v[2] == 'tag':
+                    d[k] = child[0].tag
+            else:
+                d[k] = xml.find(v[0]).get(v[1])
+        except Exception as e:
+            pass
+    return(d)
+
+
+def _parse_study_type(xml):
+    d = {}
+    if(xml.get('existing_study_type')):
+        d['study_type'] = xml.get('existing_study_type')
+    if(xml.get('new_study_type')):
+        d['study_type'] = xml.get('new_study_type')
+    return(d)
+
+
+def try_update(d, value):
+    try:
+        d.update(value)
+        return(d)
+    except Exception:
+        return(d)
+
+
+
+class SRAXMLRecord(object):
+    def __init__(self, xml):
+        #if(type(xml) is not xml.etree.ElementTree.Element):
+        #    raise(TypeError('xml should be of type xml.etree.ElementTree.Element'))
+        self.xml = xml
+        self.parse_xml()
+
+    def parse_xml(self):
+        d = {}
+        d = d.update(self.xml.attrib)
+        self.data = d
+
+
+class SRAExperimentRecord(SRAXMLRecord):
+    def __init__(self, xml):
+        super().__init__(xml)
+
+    def parse_xml(self):
+        """Parse an SRA xml EXPERIMENT element"""
+        xml = self.xml
+        self.data = parse_experiment(xml)
+
+
+class SRAStudyRecord(SRAXMLRecord):
+    def __init__(self, xml):
+        super().__init__(xml)
+
+    def parse_xml(self):
+        self.data = parse_study(self.xml)
+
+
+class SRASampleRecord(SRAXMLRecord):
+    def __init__(self, xml):
+        super().__init__(xml)
+
+    def parse_xml(self):
+        self.data = parse_sample(self.xml)
+
+
+class SRASubmissionRecord(SRAXMLRecord):
+    def __init__(self, xml):
+        super().__init__(xml)
+
+    def parse_xml(self):
+        self.data = parse_submission(self.xml)
+
+
+class SRARunRecord(SRAXMLRecord):
+    def __init__(self, xml):
+        super().__init__(xml)
+
+    def parse_xml(self):
+        self.data = parse_run(self.xml)
+
+
 class SRAExperiment(object):
     def __init__(self, node):
         """Parse SRA Experiment to dict

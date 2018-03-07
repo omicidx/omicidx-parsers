20b6851c36e101219d3f8ce3c8e26ef2b1704cac
Sean Davis <seandavi@gmail.com>
2018-03-07 06:53:22 -0500

    cleanup

--- pyspark/README.md
+++
@@ -1,3 +0,0 @@
-See [Best Practices Writing Production-Grade PySpark Jobs]
-
-[Best Practices Writing Production-Grade PySpark Jobs]: https://developerzen.com/best-practices-writing-production-grade-pyspark-jobs-cb688ac4d20f
--- scala/src/main/scala/SimpleMain.scala
+++
@@ -1,9 +0,0 @@
-import scala.xml.XML
-
-object SimpleMain {
-  def main(args: Array[String]): Unit = {
-    val x = XML.load("https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=FullXml&term=SRX000274")
-    println( ( x \\ "RUN").length )
-
-  }
-}

dab1c365dfebe42eab6f924f15c217124b61af31
Sean Davis <seandavi@gmail.com>
2018-12-19 11:24:50 -0500

    fix foreign key fixes for sra tables
    A few accessions are missing to complete
    foreign key relationships, so need to add
    those and then create foreign key constraints.

--- sql/add_foreign_keys.sh
+++ sql/add_foreign_keys.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 psql -h $PG_HOST -U $PG_USER $PG_DB <<EOF
 
+--
+-- Nasty delete hacks due to referential integrity on ORIGINAL
+-- data from SRA failing.
+--
+
+
+insert into etl.sra_experiment (accession, status)
+  (select distinct(r.experiment_accession),'[added by omicidx]' 
+      from etl.sra_run r 
+      left join etl.sra_experiment e on e.accession=r.experiment_accession 
+      where e.accession is NULL and r.experiment_accession is not NULL);
+
 alter table etl.sra_run 
 add constraint fk_sra_run_sra_experiment 
 foreign key (experiment_accession) 
 references etl.sra_experiment(accession);
 
+insert into etl.sra_sample (accession, status)
+  (select distinct(e.sample_accession),'[added by omicidx]' 
+      from etl.sra_experiment e 
+      left join etl.sra_sample s on s.accession=e.sample_accession 
+      where s.accession is NULL and e.sample_accession is not NULL);
+
+
 alter table etl.sra_experiment 
 add constraint fk_sra_experiment_sra_sample 
 foreign key (sample_accession) 
 references etl.sra_sample(accession);
+
+insert into etl.sra_study (accession, status)
+  (select distinct(e.study_accession), '[added by omicidx]' 
+      from etl.sra_experiment e 
+      left join etl.sra_study s on s.accession=e.study_accession 
+      where s.accession is NULL and e.study_accession is not null);
 
 alter table etl.sra_experiment 
 add constraint fk_sra_experiment_sra_study 

d7dae3b53dd6517c68d26dfa63e0e5a6d930e9d6
Sean Davis <seandavi@gmail.com>
2019-01-08 13:08:15 -0500

    working on master script

--- omicidx/scripts/download_files.py
+++ omicidx/scripts/download_files.py
@@ -7,26 +7,79 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 logger = logging.getLogger(__name__)
 
-@click.command()
-@click.option('--s3bucket', default="s3://omicidx.cancerdatasci.org",
-              help="The s3 bucket URL, including the s3:// part")
+@click.group(help="""Do things in this order:
+
+\b
+  1. download-mirror-files
+  2. process-xml-to-json
+  3. create on GCS
+
+And this will be wrapped.
+""")
+def cli():
+    pass
+
+@cli.command(help = """Downloads the files necessary to build
+the SRA json conversions of the XML files. """)
 @click.option('--mirrordir',
               help="The mirror directory, like NCBI_SRA_Mirroring_20190101_Full")
-def download_mirror_files(s3bucket, mirrordir):
+def download_mirror_files(mirrordir):
     logger.info('getting xml files')
     subprocess.run("wget -nH -np --cut-dirs=3 -r -e robots=off {}/{}/".format(
         "http://ftp.ncbi.nlm.nih.gov/sra/reports/Mirroring",
         mirrordir), shell = True)
 
     logger.info('getting SRA Accessions file')
-    subprocess.run("wget ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/SRA_Accessions.tab", shell = True)
+    subprocess.run("wget ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/SRA_Accessions.tab -P {}".format(mirrordir),
+                   shell = True)
 
-    logger.info('gzipping SRA Accessions file')
-    subprocess.run("gzip -c SRA_Accessions.tab | SRA_Accessions.tab", shell=True)
+import argparse
+import omicidx.sra_parsers
+import json
+import logging
+import collections
+from xml.etree import ElementTree as et
 
-    logger.info('bzipping SRA Accessions file')
-    subprocess.run("bzip2 SRA_Accessions.tab", shell=True)
+@cli.command(help="""SRA XML to JSON
 
+  Transforms an SRA XML mirroring metadata file into
+  corresponding JSON format files. JSON is line-delimited
+  JSON (not an array).""")
+@click.argument('entity')
+def process_xml_file(entity):
+
+    fname = "meta_{}_set.xml.gz".format(entity)
+
+    entity=entity
+
+    parsers = {
+        'study': omicidx.sra_parsers.SRAStudyRecord,
+        'sample': omicidx.sra_parsers.SRASampleRecord,
+        'run': omicidx.sra_parsers.SRARunRecord,
+        'experiment': omicidx.sra_parsers.SRAExperimentRecord
+    }
+    sra_parser = parsers[entity]
+
+    logger.info('using {} entity type'.format(entity))
+    logger.info('parsing {} records'.format(entity))
+    n = 0
+    outfname = "{}.json".format(entity)
+    ENTITY = entity.upper()
+    with open(outfname, 'w') as outfile:
+        with omicidx.sra_parsers.open_file(fname) as f:
+            for event, element in et.iterparse(f):
+                if(event == 'end' and element.tag == ENTITY):
+                    rec = sra_parser(element).data
+                    n+=1
+                    if((n % 100000)==0):
+                        logger.info('parsed {} {} entries'.format(entity, n))
+                    outfile.write(json.dumps(rec) + "\n")
+                    element.clear()
+            logger.info('parsed {} entity entries'.format(n))
+
+
+
+    
 if __name__ == '__main__':
-    download_mirror_files()
+    cli()
     

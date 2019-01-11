2784a69a4bda68d5299f3bc4efae9f2c61fe2145
Sean Davis <seandavi@gmail.com>
2019-01-11 15:21:34 -0500

    working biosample xml-to-json parser

--- omicidx/biosample.py
+++ omicidx/biosample.py
@@ -1,10 +1,23 @@
+"""Biosample parser
+
+Implemented as an iterator
+
+
+>>> import omicidx.biosample as b
+>>> for bios in b.BioSampleParser('biosample_set.xml.gz'):
+>>>     print(bios.as_json())
+"""
+
 import os.path
 import xml.etree.ElementTree as ET
 import gzip
 import json
 import logging
+import click
+
 
 class BioSample(dict):
+    """BioSample class"""
     def __init__(self):
         self.update()
     
@@ -18,7 +31,15 @@ class BioSample(dict):
         return('BioSample <id={0} title={1}>'.format(self['id'],self['title']))
     
 class BioSampleParser(object):
+    """Parse a BioSample xml file.
+
+    Implemented as an iterator"""
+
+    
     def __init__(self,fname):
+        """Init
+
+        Handles .gz as well as plain text"""
         self.fhandle = None
         if(fname.endswith('gz')):
             self.fhandle = gzip.open(fname)
@@ -27,10 +48,12 @@ class BioSampleParser(object):
         self.context = ET.iterparse(self.fhandle, events=("start", "end"))
         event, self.root = next(self.context)
 
+        
     def __iter__(self):
         return(self)
 
-    def next(self):
+    
+    def __next__(self):
         for event, elem in self.context:
             if event == "end" and elem.tag == "BioSample":
                 bios = BioSample()
@@ -42,11 +65,17 @@ class BioSampleParser(object):
                     idrec = {'db': id.get('db'), 'label': id.get('db_label'), 'id':id.text}
                     bios['ids'].append(idrec['id'])
                     bios['id_recs'].append(idrec)
+                    if(id.get('db')=='SRA'):
+                        bios['sra_sample']=id.text
+                    if(id.get('db')=='dbGaP'):
+                        bios['dbgap']=id.text
+                    if(id.get('db')=='GEO'):
+                        bios['gsm']=id.text
                 bios['title'] = elem.findtext('.//Description/Title')
                 bios['description'] = elem.findtext('.//Description/Comment/Paragraph')
                 organism = elem.find('.//Organism')
                 bios['taxonomy_name'] = organism.get('taxonomy_name')
-                bios['taxonomy_id'] = organism.get('taxonomy_id')
+                bios['taxon_id'] = int(organism.get('taxonomy_id'))
                 bios['attribute_recs'] = []
                 bios['attributes'] = []
                 for attribute in elem.findall('.//Attribute'):
@@ -57,10 +86,19 @@ class BioSampleParser(object):
                         bios['attributes'].append(attrec['harmonized_name'])
                     except:
                         bios['attributes'].append(attrec['attribute_name'])
+                        
                 bios['model'] = elem.findtext('.//Model')
                 #print(json.dumps(bios))
                 #res = es.index(index="bioes", doc_type='biosample', id=bios['id'], body=bios)
                 elem.clear()
-                return(bios)
+                return bios
 
 
+@click.command("""Parse biosample xml file to json and dump to stdout""")
+@click.argument('biosample_file')
+def cli(biosample_file):
+    for i in BioSampleParser(biosample_file):
+        print(i.as_json())
+
+if __name__ == '__main__':
+    cli()

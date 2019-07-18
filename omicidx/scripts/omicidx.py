63624cac6b0131db5c376244c8447097980ee8ce
Sean Davis <seandavi@gmail.com>
2019-07-17 21:58:02 -0400

    working asyncio, but not multiprocessing

--- omicidx/scripts/omicidx.py
+++ omicidx/scripts/omicidx.py
@@ -2,6 +2,13 @@ import click
 import omicidx.biosample
 import sd_cloud_utils.aws.sqs as sqs
 import omicidx.sra_parsers as sp
+import logging
+import json
+import datetime
+import asyncio
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('omicidx-cli')
 
 @click.group('biosample')
 def biosample():
@@ -47,28 +54,163 @@ def sra_load2bigquery():
 @click.group(help='get SRA metadata as json')
 @click.option('--config',
               help="config file")
-def json(config=None):
+def json_utils(config=None):
     pass
 
 
-@json.command(help='Convert SRR to json')
+def dateconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+def _srr_to_json(accession):
+    models = sp.models_from_runbrowser(accession)
+    if(not isinstance(models, dict)):
+        return None
+    res = {}
+    for k in models.keys():
+        res[k] = models[k].dict()
+    print(json.dumps(res, default = dateconverter))
+
+@json_utils.command(help='Convert SRR to json')
 @click.option('--accession',
               help = "An SRA Run accession")
 def srr_to_json(accession):
-    models = sp.models_from_runbrowser(run_accession)
-    res = {}
-    for k in models.keys():
-        res[k] = models[k].json()
-    print(res)
-
+    _srr_to_json(accession)
     
 @click.group()
 def omicidx_cli():
     pass
 
+@click.group()
+def admin():
+    pass
+
+@admin.command()
+@click.option('--queue')
+def create_queue(queue):
+    sqs = sqs.SQS(queuename=queue)
+    sqs.get_queue()
+    return True
+
+@admin.command()
+@click.option('--queue')
+@click.option('--message')
+@click.option('--from_date')
+@click.option('--to_date')
+def send_message(queue, message, from_date, to_date):
+    n = 0
+    q = sqs.SQS(queue)
+    msg = []
+    for rec in sp.get_accession_list(
+            from_date = from_date,
+            to_date = to_date,
+            type = 'RUN'
+    ):
+        msg.append(q.make_message(body=rec['Accession'], id=str(n)))
+        n+=1
+        if(n % 10 == 0):
+            q.send_messages(msg)
+            msg = []
+            logger.info('sent {} messages'.format(n))
+    q.send_messages(msg)
+    logger.info('sent {} messages'.format(n))
+    
+
+
+async def produce(queue, queuename):
+    while True:
+        vals = sqs.SQS(queuename=queuename).receive_message()
+        if(len(vals)==0):
+            logger.info('nothing to retrieve')
+            queue.close()
+        for val in vals:
+            logger.info('Message:' + val.body)
+            await queue.put(val)
+
+
+def accession_iterator(queuename):
+    n = 0
+    while n<10:
+        n+=1
+        vals = sqs.SQS(queuename=queuename).receive_message()
+        if(len(vals)==0):
+            logger.info('nothing to retrieve')
+            return
+        for val in vals:
+            logger.info('Message: ' + val.body)
+            yield(val)
+
+
+def process_one_accession(val):
+    models = sp.models_from_runbrowser(val.body)
+    if(isinstance(models, dict)):
+        res = {}
+        for k in models.keys():
+            res[k] = models[k].dict()
+        val.delete()
+        print(json.dumps(res, default = dateconverter))
+
+@admin.command()
+@click.option('--cpus', type=int)
+@click.option('--queue')
+def process_accessions(queue, cpus):
+    from multiprocessing import Pool
+    pool = Pool(cpus)
+    pool.map(process_one_accession, accession_iterator(queue))
+    
+
+
+async def consume(queue):
+    while True:
+        # wait for an item from the producer
+        val = await queue.get()
+        
+        # process the item
+        models = sp.models_from_runbrowser(val.body)
+        if(isinstance(models, dict)):
+            res = {}
+            for k in models.keys():
+                res[k] = models[k].dict()
+            val.delete()
+            queue.task_done()
+            print(json.dumps(res, default = dateconverter))
+        else:
+            # Notify the queue that the item has been processed
+            val.delete()
+            queue.task_done()
+
+
+async def run(queuename):
+    queue = asyncio.Queue(maxsize=10)
+    # schedule the consumer
+    consumer = asyncio.ensure_future(consume(queue))
+    # run the producer and wait for completion
+    await produce(queue, queuename)
+    # wait until the consumer has processed all items
+    await queue.join()
+    # the consumer is still awaiting for an item, cancel it
+    consumer.cancel()
+
+
+
+
+
+            
+@admin.command()
+@click.option('--queue')
+def process_accessions_asyncio(queue):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(queue))
+    loop.close()
+
+
+
 omicidx_cli.add_command(biosample)
 omicidx_cli.add_command(sra)
-omicidx_cli.add_command(json)
+omicidx_cli.add_command(json_utils)
+omicidx_cli.add_command(admin)
+
+
 
 if __name__ == '__main__':
     omicidx_cli()

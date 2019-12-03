96a1182fc244c0f5bcdc08e8b49f6311de303499
Sean Davis <seandavi@gmail.com>
2019-12-02 22:09:43 -0500

    sra runbrowser model is pydantic model
    * fixes tests

--- tests/sra/parser/test_parser_runbrowser_run_model.py
+++ tests/sra/parser/test_parser_runbrowser_run_model.py
@@ -11,25 +11,25 @@ def example_runbrowser_model():
     return s.models_from_runbrowser(EXAMPLE_SRR)
 
 def test_runbrowser_result_has_keys(example_runbrowser_model):
-    """runbrowser model has four keys"""
-    assert len(example_runbrowser_model.keys())==4
-    for i in 'run sample study experiment'.split():
+    """runbrowser model has study, sample, experiment records"""
+    example_runbrowser_model = example_runbrowser_model.dict()
+    for i in 'sample study experiment'.split():
         assert i in example_runbrowser_model.keys()
 
 def test_runbrowser_run_has_qualities(example_runbrowser_model):
-    run = example_runbrowser_model['run']
+    run = example_runbrowser_model
     assert len(run.qualities) > 0
 
 def test_runbrowser_run_has_tax_analysis(example_runbrowser_model):
-    run = example_runbrowser_model['run']
+    run = example_runbrowser_model
     assert run.tax_analysis is not None
 
 def test_runbrowser_run_accession_matches(example_runbrowser_model):
-    run = example_runbrowser_model['run']
+    run = example_runbrowser_model
     assert run.accession == EXAMPLE_SRR
 
 def test_runbrowser_run_has_entries(example_runbrowser_model):
-    run = example_runbrowser_model['run']
+    run = example_runbrowser_model
     assert len(run.files)>0
     assert len(run.base_counts)>0
     assert len(run.tax_analysis.tax_counts) > 0
@@ -38,7 +38,7 @@ def test_runbrowser_run_has_entries(example_runbrowser_model):
     assert run.cluster_name == "public"
 
 def test_runbrowser_livelist_entries(example_runbrowser_model):
-    run = example_runbrowser_model['run']
+    run = example_runbrowser_model
     assert isinstance(run.published, datetime.datetime)
     assert isinstance(run.received, datetime.datetime) or run.received is None 
     assert isinstance(run.lastupdate, datetime.datetime) or run.lastupdate is None 

fa3292bdbda3c4c4065f8c8d81bbc51cb10ebf53
Sean Davis <seandavi@gmail.com>
2019-12-20 09:32:49 -0500

    add better retry handling
    Only partially implemented, but from
    https://www.peterbe.com/plog/best-practice-with-retries-with-requests
    implemented better retry support. Still needs
    to be plugged in to all url requests, but done
    for runbrowser here.

--- omicidx/utils.py
+++ omicidx/utils.py
@@ -1,6 +1,11 @@
 import gzip
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from typing import Tuple
 
-def open_file(fname, mode = 'rt'):
+
+def open_file(fname: str, mode: str = 'rt'):
     """open a file, including dealing with gzipped files
  
     Parameters
@@ -17,3 +22,57 @@ def open_file(fname, mode = 'rt'):
     else:
         f = open(fname, mode)
     return(f)
+
+
+def requests_retry_session(
+    retries: int = 3,
+    backoff_factor: float = 0.3,
+    status_forcelist: Tuple[int]=(500, 502, 504),
+    session: requests.Session = None,
+) -> requests.Session:
+    """Get a requests session that handles retries
+
+    Parameters
+    ==========
+    retries: int
+        Number of retries allowed
+    backoff_factor: float
+        Implements geometric backoff 
+    status_forcelist: Tuple[int]
+        Status codes to retry for
+    session: requests.Session 
+        Supply a session that will be modified, or 
+        a new session will be created.
+        
+    Returns
+    =======
+    A new requests.Session with retries enforced.
+    ```
+t0 = time.time()
+try:
+    response = requests_retry_session().get(
+        'http://httpbin.org/delay/10',
+        timeout=5
+    )
+except Exception as x:
+    print('It failed :(', x.__class__.__name__)
+else:
+    print('It eventually worked', response.status_code)
+finally:
+    t1 = time.time()
+    print('Took', t1 - t0, 'seconds')
+    ```
+    """
+
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session

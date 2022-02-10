edb0e5c08c09cec6fda9596ec77695fac8c85ee7
Sean Davis <seandavi@gmail.com>
2022-02-09 21:35:59 -0700

    need to test ontologies using owl version

--- omicidx/ontologies/test_ontology_utils.py
+++ omicidx/ontologies/test_ontology_utils.py
@@ -11,7 +11,7 @@ def test_ids_in_get_all_ontologies_from_obo_library():
 
 
 def test_get_an_ontology():
-    cl = ontology_from_obo_library('cl.obo')
+    cl = ontology_from_obo_library('cl.owl')
     assert 'CL:0000900' in cl
     assert isinstance(cl, pronto.Ontology)
     cl_inst = cl['CL:0000900']

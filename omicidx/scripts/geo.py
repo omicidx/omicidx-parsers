c9be015f8df8b781887216ff745c440533a5c21c
Sean Davis <seandavi@gmail.com>
2022-06-04 19:05:20 -0600

    Change GEO cli a bit to be more flexible

--- omicidx/scripts/geo.py
+++ omicidx/scripts/geo.py
@@ -1,6 +1,9 @@
 import omicidx.geo.parser as op
 import click
 import json
+import sys
+from datetime import date
+
 
 @click.group()
 def cli():
@@ -14,12 +17,13 @@ def gse_to_json(gse):
     print(op.gse_to_json(gse))
 
 @cli.command()
-@click.option('--term',
-              help='something like "2019/01/28:2019/02/28[PDAT]"')
-@click.option("--outfile", type=click.File('w'),
-              help="output file name, default is stdout")
-def bulk_gse_to_json(term, outfile = '/dev/stdout'):
-    op.bulk_gse_to_json(term, outfile)
+@click.argument('etype', type=click.Choice(["GSE","GPL","GSM"]),default="GSE")
+@click.option('--date-start', type=click.DateTime(formats=["%Y-%m-%d"]),default=str(date.today()))
+@click.option('--date-end', type=click.DateTime(formats=["%Y-%m-%d"]),default=str(date.today()))
+def bulk_gse_to_json( date_start: date, date_end: date, etype: str='GSE'):
+    date_formatter = lambda x: x.strftime("%Y/%m/%d")
+    term = f"{date_formatter(date_start)}:{date_formatter(date_end)}[PDAT] AND {etype}[ETYP]"
+    op.bulk_gse_to_json(term, sys.stdout)
 
 
 @cli.command()
